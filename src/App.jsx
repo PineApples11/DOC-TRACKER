a8890fd735f0683f1bcb9f9b5641b4bacd06d075
Maidan10 <aidan.marube@gmail.com>
2025-02-01 12:16:12 +0300

    Appointment Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import './App.css'
 import PatientForm from './PatientForm'
+import Appointmentform from './Appointment'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Appointmentform/>
    <PatientForm/>
     </>
   )
